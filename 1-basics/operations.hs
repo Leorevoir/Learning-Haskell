0432e3a9afffe547564cecd37251faa59a0471b2
Leorevoir <leoquinzler@epitech.eu>
2024-07-18 12:07:55 +0200

    add: 1-basics exercises

---
+++ 1-basics/operations.hs
@@ -0,0 +1,47 @@
+{--
+Basic operations in Haskell
+
+All functions support arithmetic operations on integers or floats.
+--}
+
+-- add integers or float
+addNum :: Num a => a -> a -> a
+addNum x y = x + y
+
+
+-- subtract integers or float
+subNum :: Num a => a -> a ->a
+subNum x y = x - y
+
+
+-- multiply integers or float
+multNum :: Num a => a -> a -> a
+multNum x y = x * y
+
+
+-- divide integers or float
+divNum :: (Num a, Eq a, Fractional a) => a -> a -> a
+divNum x y
+    | y == 0 = error "Division by zero"
+    | otherwise = x / y
+
+
+-- sum of squares
+sumOfSquares :: Num a => a -> a -> a
+sumOfSquares x y = addNum (x * x) (y * y)
+
+
+-- returns last digit
+-- only works with int
+lastDigit :: Int -> Int
+lastDigit x = mod (abs x) 10
+
+
+-- function that finds a product of all the numbers in the list
+-- if the function sees a 0, it returns 0 immediately
+listProduct :: [Int] -> Int
+listProduct [] = 1
+listProduct (x:xs)
+    | x == 0 = 0
+    | otherwise = x * listProduct xs
+
---
+++ 1-basics/string.hs
@@ -0,0 +1,40 @@
+import Distribution.Text (display)
+import Distribution.Compat.CharParsing (lower)
+{--
+Basic string operations in Haskell
+--}
+
+
+-- function that takes start, end positions and string
+-- return the substring
+subString :: Int -> Int -> [Char] -> [Char]
+subString start end str
+    | end < 0   = ""
+    | start < 0 = take (end + 1) str
+    | otherwise = take (end - start + 1) (drop start str)
+
+
+-- function that returns the sum of the string
+sumString :: [Char] -> Int
+sumString str = sum (map read (words str))
+
+
+-- function that duplicates every inputs
+duplicate :: [a] -> [a]
+duplicate [] = []
+duplicate (x:xs) = x : x : duplicate xs
+
+
+-- function that takes an int, list of int and returns a string
+-- saying how many elements of the list are strictly
+-- > and < than the given int
+lowerAndGreater :: Int -> [Int] -> String
+lowerAndGreater n xs =
+    let lower_than = length (filter (< n) xs)
+        greater_than = length (filter (> n) xs)
+    in show n ++ " is greater than "
+      ++ show lower_than
+      ++ " elements and lower than "
+      ++ show greater_than
+      ++ " elements."
+
